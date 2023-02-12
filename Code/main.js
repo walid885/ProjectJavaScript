bfd8bd2a649e75799afd1add4962d7f468005af5
walid885 <bettaieb88walid@gmail.com>
2023-02-13 00:04:24 +0100

    comments in order to rediscover JS

--- Code/main.js
+++ Code/main.js
@@ -3,3 +3,10 @@ console.log("Hello world! ")
 // semi coloumns are not mandotry 
 const name ="john"
 const age = 30 ; 
+//using the plus sign to concatenate string 
+// template literale , ES6/2015 using backticks ``
+
+const s = ' HELLO world '
+console.log(s.length);
+//s.susbtring, S.toupper() ; all diifrent , string manipulation,  s.Split("/*this is the caaractere that we are going to use in order to split*/")
+ 
